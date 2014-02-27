5bf8e38c514420173085cf38a1f070e110dcee48
itchyny <neknekneknek.t@gmail.com>
2014-02-27 18:34:08 +0900

    add Bf.hs

---
+++ Language/Brainfuck/Py2Bf/Bf.hs
@@ -0,0 +1,40 @@
+module Language.Brainfuck.Py2Bf.Bf
+  ( Bf (..)
+  , Bfcode (..)
+  ) where
+
+import Data.List (intercalate)
+import Language.Brainfuck.Py2Bf.Bfprim
+
+class Bf a where
+  convert :: a -> Bfcode
+  upconvert :: Bfcode -> a
+
+data Bfcode = Bfcode [Bfprim]
+            deriving Eq
+
+instance Show Bfcode where
+  show (Bfcode bf) = intercalate "\n" (spl 80 (show (compress bf)))
+
+instance Read Bfcode where
+  readsPrec _ str = [(Bfcode (read str :: [Bfprim]), "")]
+
+spl :: Int -> [a] -> [[a]]
+spl d xs | null rs   = [r]
+         | otherwise = r : spl d rs
+  where (r, rs) = splitAt d xs
+
+compress :: [Bfprim] -> [Bfprim]
+compress = trailingNonIO . succWhile . headingWhile
+
+succWhile :: [Bfprim] -> [Bfprim]
+succWhile (BfWhile bff : bf)
+  = BfWhile (succWhile bff) : succWhile (dropWhile isBfWhile bf)
+succWhile (b : bf) = b : succWhile bf
+succWhile [] = []
+
+headingWhile :: [Bfprim] -> [Bfprim]
+headingWhile = dropWhile isBfWhile
+
+trailingNonIO :: [Bfprim] -> [Bfprim]
+trailingNonIO = reverse . dropWhile isBfprimNonIO . reverse
---
+++ test/Language/Brainfuck/Py2Bf/BfSpec.hs
@@ -0,0 +1,57 @@
+module Language.Brainfuck.Py2Bf.BfSpec where
+import Test.Hspec
+import Test.QuickCheck
+import Language.Brainfuck.Py2Bf.Bf
+
+spec :: Spec
+spec = do
+  describe "Bf" $ do
+    show_spec
+    read_spec
+    skip_spec
+    show_read_spec
+
+show_spec :: Spec
+show_spec =
+  it "shows Bfcode correctly" $ do
+    property (\n -> let m = n `mod` 1000 in
+                        filter (/='\n') (show (Bfcode (replicate m BfIncr ++ [BfPut])))
+                                                    == replicate m '+' ++ ".")
+
+read_spec :: Spec
+read_spec =
+  it "reads Bfcode correctly" $ do
+    read "+++" `shouldBe` Bfcode [BfIncr, BfIncr, BfIncr]
+    read "---" `shouldBe` Bfcode [BfDecr, BfDecr, BfDecr]
+    read ">>>" `shouldBe` Bfcode [BfNext, BfNext, BfNext]
+    read "<<<" `shouldBe` Bfcode [BfPrev, BfPrev, BfPrev]
+    read "..." `shouldBe` Bfcode [BfPut, BfPut, BfPut]
+    read ",,," `shouldBe` Bfcode [BfGet, BfGet, BfGet]
+    read "[][][]" `shouldBe` Bfcode [BfWhile [], BfWhile [], BfWhile []]
+    read "[+++--->>><<<]" `shouldBe`
+      Bfcode [BfWhile
+        [BfIncr, BfIncr, BfIncr, BfDecr, BfDecr, BfDecr,
+         BfNext, BfNext, BfNext, BfPrev, BfPrev, BfPrev]]
+
+skip_spec :: Spec
+skip_spec =
+  it "skips whites" $ do
+    read "   +   +   +   " `shouldBe`
+      Bfcode [BfIncr, BfIncr, BfIncr]
+    read "\n\n[\t\t]  [\n\n]\t\t[  ]\n\n" `shouldBe`
+      Bfcode [BfWhile [], BfWhile [], BfWhile []]
+    read "  [\t\t+\n+\n+\n--\n->\t>>\t<<\t<]" `shouldBe`
+      Bfcode [BfWhile
+        [BfIncr, BfIncr, BfIncr, BfDecr, BfDecr, BfDecr,
+         BfNext, BfNext, BfNext, BfPrev, BfPrev, BfPrev]]
+
+show_read_spec :: Spec
+show_read_spec =
+  it "show . read" $ do
+    show (read "" :: Bfcode) `shouldBe` ""
+    show (read "[]" :: Bfcode) `shouldBe` ""
+    show (read "+++" :: Bfcode) `shouldBe` ""
+    show (read "+++[+++]" :: Bfcode) `shouldBe` ""
+    show (read "+++[>+++<-]>" :: Bfcode) `shouldBe` ""
+    show (read "+++[>+++<-]>." :: Bfcode) `shouldBe` "+++[>+++<-]>."
+    show (read "+++[>+++<-][>>][<<][++]>." :: Bfcode) `shouldBe` "+++[>+++<-]>."
