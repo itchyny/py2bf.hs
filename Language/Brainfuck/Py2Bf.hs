bfef01ff491b1ae86a517575ff08584aaeb0a0f3
itchyny <neknekneknek.t@gmail.com>
2014-03-01 21:01:12 +0900

    improve Py2Bf: import Language.Brainfuck.Py2Bf.Bf

--- Language/Brainfuck/Py2Bf.hs
+++ Language/Brainfuck/Py2Bf.hs
@@ -1,5 +1,7 @@
 module Language.Brainfuck.Py2Bf
   ( module Language.Brainfuck.Py2Bf.Bfprim
+  , module Language.Brainfuck.Py2Bf.Bf
   ) where
 
 import Language.Brainfuck.Py2Bf.Bfprim
+import Language.Brainfuck.Py2Bf.Bf
